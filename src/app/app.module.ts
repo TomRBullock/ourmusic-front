15be29e8ad0db5769812525e72fda34bb2a7b564
Thomas <thomas@lindar.com>
2020-02-10 17:33:29 +0000

    Add styling and theming. Begin page work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularMaterialModule } from './angular-material.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+// import { MatButtonToggleModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    // MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
